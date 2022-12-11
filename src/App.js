10cf7d0dbcec6415fd87d902e419ac8a8a995492
farrar <thaobaan@gmail.com>
2022-12-11 21:31:39 +0100

    changed initial url

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import WebViewer from "@pdftron/webviewer";
 
 // https://pdf.ac/C1NoX
 
+// "https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf"
+
 const App = () => {
   const viewer = useRef(null);
 
@@ -11,7 +13,7 @@ const App = () => {
       {
         path: "lib",
         initialDoc:
-          "https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf",
+          "https://carleton.ca/financialservices/wp-content/uploads/sample_signature_form_carleton_u_v5.pdf",
       },
       viewer.current
     ).then((instance) => {});
